2375022cdf6938880670fb515ba263514ded7d01
Gantt-sucessor <javarealfan@gmail.com>
2026-05-19 17:29:25 -0400

    feat: Primeira migration criada

--- src/main/resources/db/migration/V1__criar_tabelas_iniciasi.sql
+++ src/main/resources/db/migration/V1__criar_tabelas_iniciasi.sql
@@ -2,5 +2,6 @@ create table profissionais(
     id primary key not null,
     nome_completo varchar(255) not null,
     email varchar(255) not null,
+    senha varchar(255) not  null
     ativo boolean not null
 );
