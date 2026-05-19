9dade200df9d2b1a79274be40d6b092ed31dde07
Gantt-sucessor <javarealfan@gmail.com>
2026-05-19 17:13:23 -0400

    feat: Testando mexer com migration

--- src/main/resources/db/migration/V1__criar_tabelas_iniciasi.sql
+++ src/main/resources/db/migration/V1__criar_tabelas_iniciasi.sql
@@ -1,4 +1,6 @@
 create table profissionais(
     id primary key not null,
-    full_name varchar(255) not null
+    nome_completo varchar(255) not null,
+    email varchar(255) not null,
+    ativo boolean not null
 );
