01e66866f2f7cc6ed87e710a6faf68094eedc731
Gantt-sucessor <javarealfan@gmail.com>
2026-05-19 19:20:12 -0400

    feat: Migration de responsaveis e arrumando erro de escrita na migration V1

--- src/main/resources/db/migration/V1__criar_tabelas_iniciasi.sql
+++ src/main/resources/db/migration/V1__criar_tabelas_iniciasi.sql
@@ -1,7 +1,7 @@
 create table profissionais(
-    id primary key not null,
+    id int primary key not null,
     nome_completo varchar(255) not null,
     email varchar(255) not null,
-    senha varchar(255) not  null
+    senha varchar(255) not  null,
     ativo boolean not null
 );
--- src/main/resources/db/migration/V2__criar_tabela_responsaveis.sql
+++ src/main/resources/db/migration/V2__criar_tabela_responsaveis.sql
@@ -1,5 +1,5 @@
 create table responsaveis(
-    id primary key not null,
+    id int primary key not null,
     nome_completo varchar(200) not null,
     cpf varchar(255) not null,
     data_nascimento date not null
